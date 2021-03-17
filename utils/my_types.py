a3c52dcda19b31d88dae6a86c156cfb6b2c53652
Romain Cassan <romain.cassan@autajon.com>
2021-03-17 10:48:39 +0100

    Add None to possible types for CellValue

--- utils/my_types.py
+++ utils/my_types.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Defines types commonly used."""
+
+
 from typing import Union, Dict, Tuple, List
 
 import xlwings
 
+
 Row = int
 Column = int
-CellValue = Union[int, str]
+CellValue = Union[int, str, None]
 DataRow = Dict[str, CellValue]
 ExcelTable = xlwings._xlwindows.COMRetryObjectWrapper
 ColumnFilter = Tuple[int, List[CellValue]]
